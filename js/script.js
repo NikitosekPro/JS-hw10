072f7c56a3c278b8edcd8bacd90320d972982f91
NikitosekPro <148691930+NikitosekPro@users.noreply.github.com>
2025-02-22 19:58:00 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
 // Task 1
-const showMessage = () => console.log("Hello World!");
-showMessage();
+// const showMessage = () => console.log("Hello World!");
+// showMessage();
 
 // Task 2
-let randomNumber = Math.floor(Math.random() * 100) + 1;
-const checkGuess = (userGuess) => {
-    return userGuess == randomNumber ? "Правильно!" : "Неправильно.";
-};
-console.log(checkGuess(50));
+// let randomNumber = Math.floor(Math.random() * 100) + 1;
+// const checkGuess = (userGuess) => {
+//     return userGuess == randomNumber ? "Правильно!" : "Неправильно.";
+// };
+// console.log(checkGuess(50));
 
 // Task 3
-let clickCount = 0;
-const countClicks = () => {
-    clickCount++;
-    console.log(`Кількість кліків: ${clickCount}`);
-};
-countClicks();
+// let clickCount = 0;
+// const countClicks = () => {
+//     clickCount++;
+//     console.log(`Кількість кліків: ${clickCount}`);
+// };
+// countClicks();
 
 // Task 4
-const applyCallbackToEachElement = (arr, callback) => arr.map(callback);
-const arr = [1, 2, 3, 4, 5];
-const squareCallback = num => num * num;
-const result = applyCallbackToEachElement(arr, squareCallback);
-console.log(result); // [1, 4, 9, 16, 25]
+// const applyCallbackToEachElement = (arr, callback) => arr.map(callback);
+// const arr = [1, 2, 3, 4, 5];
+// const squareCallback = num => num * num;
+// const result = applyCallbackToEachElement(arr, squareCallback);
+// console.log(result); // [1, 4, 9, 16, 25]
 
 // Task 5
 const calculateDiscountedPrice = (price, discount, callback) => {
